4ae3aac9a85bf8f032b63725955943c1e343dde6
Olaosebikan Habeebullah <63933925+udayfah@users.noreply.github.com>
2020-06-08 16:16:27 +0100

    Add files via upload
    FROM HNG ID-05079

---
+++ index.js
@@ -0,0 +1,29 @@
+// index.js
+
+/**
+ * Required External Modules
+ */
+const express = require("express");
+const path = require("path");
+/**
+ * App Variables
+ */
+const app = express()
+const port = process.env.PORT || "8000";
+/**
+ *  App Configuration
+ */
+
+/**
+ * Routes Definitions
+ */
+app.get("/", (req, res) => {
+    res.status(200).send("HNG TASK 2");
+  });
+/**
+ * Server Activation
+ */
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+  });
+
