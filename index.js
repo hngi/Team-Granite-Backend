e19f2a79309d391b3000f0ec82bf49dd1caeb552
Olaosebikan Habeebullah <63933925+udayfah@users.noreply.github.com>
2020-06-08 22:28:08 +0100

    From udayfah

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const path = require("path");
+const app = express()
+const port = process.env.PORT || "8000";
+app.get("/", (req, res) => {
+    res.status(200).send("HNG TASK 2");
+  });
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+  });
+
