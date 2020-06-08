a8a8fcc0079ee250586031d963c9728d58ab9a68
Olaosebikan Habeebullah <63933925+udayfah@users.noreply.github.com>
2020-06-08 22:42:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require("path");
 const app = express()
 const port = process.env.PORT || "8000";
 app.get("/", (req, res) => {
-    res.status(200).send("HNG TASK 2");
+    res.status(200).send("HNG TASK 2 for udayfah");
   });
 app.listen(port, () => {
     console.log(`Listening to requests on http://localhost:${port}`);
