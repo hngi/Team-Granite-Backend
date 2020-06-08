75b9bcd5eed93a592c5a28ffbc98c9bd3da051ac
tochukwu chinedu <tochukwuchinedu21@gmail.com>
2020-06-08 16:51:02 +0100

    Integrated Mongo and Mongoose

---
+++ src/models/user.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+
+const User = mongoose.model('User', {
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value){
+            if (!validator.isEmail(value)){
+                //subject to change to a custom response later
+                throw new Error('Invalid Email');
+            }
+        }
+    },
+    phoneNumber: {
+        type: Number,
+        required: true,
+        validate(value){
+            if (value.length < 11 || value.length > 13){
+                //subject to change to a custom response later
+                throw new Error('Invalid Phone Number');
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0
+    }
+})
+
+module.exports = User
