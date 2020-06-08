5f167e9d9b13ca06b46b7dd714c5ac4b74862899
Olaosebikan Habeebullah <63933925+udayfah@users.noreply.github.com>
2020-06-08 16:31:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,12 @@
 // index.js
 
-/**
- * Required External Modules
- */
 const express = require("express");
 const path = require("path");
-/**
- * App Variables
- */
 const app = express()
 const port = process.env.PORT || "8080";
-/**
- *  App Configuration
- */
-
-/**
- * Routes Definitions
- */
 app.get("/", (req, res) => {
     res.status(200).send("HNG TASK 2");
   });
-/**
- * Server Activation
- */
 app.listen(port, () => {
     console.log(`Listening to requests on http://localhost:${port}`);
   });
-
