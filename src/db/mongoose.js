f864d4218abee9ab09239e075ea08d7d11364667
Marvellous Ubani <marvellousubani@gmail.com>
2020-06-09 04:09:04 +0100

    added useFindAndModify param, also set to the mongo connect link to a hosted db

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,11 +1,15 @@
 import mongoose from 'mongoose';
 
 const connectToDatabase = () => {
-    mongoose.connect('mongodb://127.0.0.1:27017/Team-Granite-Dockerized-Users-Management-App-db',{
+    mongoose.connect('mongodb://team-granite:granite1@ds047065.mlab.com:47065/team-granite',{
         useUnifiedTopology: true,
         useNewUrlParser: true,
-        useCreateIndex: true
+        useCreateIndex: true,
+        useFindAndModify: false
     });
+    mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
 };
+
+// mongodb://127.0.0.1:27017/Team-Granite-Dockerized-Users-Management-App-db
 
 export default connectToDatabase;
