af2d807c73331022c9f39200db4ac8098f03987c
papa28x4 <swissfair@yahoo.co.uk>
2020-06-10 06:22:48 +0100

    added more controls to the user model

--- src/models/user.js
+++ src/models/user.js
@@ -16,6 +16,7 @@ const User = mongoose.model('User', {
     email: {
         type: String,
         required: true,
+        unique: true,
         trim: true,
         lowercase: true,
         validate(value){
@@ -29,15 +30,23 @@ const User = mongoose.model('User', {
         type: String,
         required: true,
         validate(value){
-            if (value.length < 11 || value.length > 13){
+                if(!/^\d{11,13}$/.test(value)){
+                    if(/[^\d]/.test(value)){
+                        throw new Error('Invalid Characters. Only numbers are allowed');
+                    }
                 //subject to change to a custom response later
-                throw new Error('Invalid Phone Number');
+                throw new Error('Phone number should consist of 11 to 13 numbers');
             }
         }
     },
     age: {
         type: Number,
-        default: 0
+        default: 0,
+        validate(value){
+			if(value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+		}
     }
 })
 
