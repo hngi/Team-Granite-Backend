9d12b60c495567f3fa6ae3174e285e917581056f
EkeneFidel <echukwurah99@gmail.com>
2020-06-10 01:25:33 +0100

    corrected change to email in user.js

--- src/models/user.js
+++ src/models/user.js
@@ -1,45 +1,48 @@
 import mongoose from 'mongoose';
 import validator from 'validator';
 
-
 const User = mongoose.model('User', {
-    firstName: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    lastName: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        
-    },
-    phoneNumber: {
-        type: String,
-        required: true,
-        validate(value){
-            if (value.length < 11 || value.length > 13){
-                //subject to change to a custom response later
-                throw new Error('Invalid Phone Number');
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0
-    },
-    gender: {
-        type: String,
-        required: true,
-        trim: true,
-        
-    }
-})
+	firstName: {
+		type: String,
+		required: true,
+		trim: true
+	},
+	lastName: {
+		type: String,
+		required: true,
+		trim: true
+	},
+	email: {
+		type: String,
+		required: true,
+		trim: true,
+		lowercase: true,
+		validate(value) {
+			if (!validator.isEmail(value)) {
+				//subject to change to a custom response later
+				throw new Error('Invalid Email');
+			}
+		}
+	},
+	phoneNumber: {
+		type: String,
+		required: true,
+		validate(value) {
+			if (value.length < 11 || value.length > 13) {
+				//subject to change to a custom response later
+				throw new Error('Invalid Phone Number');
+			}
+		}
+	},
+	age: {
+		type: Number,
+		default: 0
+	},
+	gender: {
+		type: String,
+		required: true,
+		trim: true
+	}
+});
 
 export default User;
