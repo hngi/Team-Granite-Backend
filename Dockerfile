9f022fb10c3343b7fe107d6f06e59bf87cab3ce4
Ruberandinda Patience <ruberandindap@gmail.com>
2020-06-10 23:57:06 +0200

    allow swagger in docker

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM node:10-alpine
 
 RUN mkdir -p /home/node/app/node_modules && chown -R node:node /home/node/app
 
+RUN chmod -R ugo+w /usr/local/lib/node_modules
+
 WORKDIR /home/node/app
 
 COPY package*.json ./
@@ -10,7 +12,12 @@ USER node
 
 RUN npm install
 
+RUN npm install -g swagger-ui-express
+
 COPY --chown=node:node . .
+
+#RUN ls -lh /usr/local/bin/docker-entrypoint.sh
+#RUN cat /usr/local/bin/docker-entrypoint.sh
 
 EXPOSE 5000
 
--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 const helmet = require('helmet');
 const router =  require('./src/routes/routes');
 const openApiDocumentation = require('./src/swagger/openApiDocumentation');
-// const swaggerUi = require('swagger-ui-express');
+const swaggerUi = require('swagger-ui-express');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -15,7 +15,7 @@ const connectToDatabase = require('./db');
 dotenv.config();
 // connectToDatabase(); //This removed to simplify the connection
 
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiDocumentation));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiDocumentation));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
