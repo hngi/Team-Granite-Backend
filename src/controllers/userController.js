bd4c68f8df99510f7a2a93774ef2de3581364949
Yusuf-AH <66280341+Yusuf-AH@users.noreply.github.com>
2020-06-10 21:10:54 +0100

    Custom responses for all

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -21,91 +21,89 @@ const user = {
     addUser: (req, res) => {
         const { firstName, lastName, email, phone, age, status, address, gender } = req.body;
         const newUser = new userModel({firstName, lastName, email, phone, age, status, address, gender});
-        newUser.save().then(() => res.json({'Status': 'Success', 'Message': 'New user created!', 'Data': user}))
-        .catch( err => res.status(400).json({'Status': 'Failed', 'Message': 'Error: ' + err, 'Data': ''}));
+        newUser.save().then(() => res.json({status: 'Success', message: 'New user created!', data: user}))
+        .catch( err => res.status(400).json({status: 'Failed', message: err, data: null}));
+    },
+    removeUser: (req, res) =>{
+        userModel.findByIdAndDelete({_id: req.params.id}).then(()=> res.status(200).json({status: 'Success', message: 'User removed!', data: null}))
+        .catch((err) => res.status(400).json({status: 'Failed', message: err, data: null}));
     },
     setUserFirstName: async (req, res) => {
         const user = await userModel.findOneAndUpdate({ _id: req.params.id }, {firstName : req.body.firstName});
-        res.status(200).send(user);
+        res.status(200).json({status: 'Success', message: 'First name updated!', data: user});
     },
 
     setUserLastName: async (req, res) => {
         const user = await userModel.findOneAndUpdate({ _id: req.params.id }, {lastName : req.body.lastName});
-        res.status(200).send(user);
+        res.status(200).json({status: 'Success', message: 'Last name updated!', data: user});
+    },
+    getUserFirstName: (req, res) =>{
+        userModel.findOne({_id: req.params.id}).then(user => res.json({status: 'Success', message: 'User first name', data: firstName}))
+        .catch((err) => res.status(400).json({status: 'Failed', message: err, data: null}));
+    },
+    getUserLastName: (req, res) =>{
+        userModel.findOne({_id: req.params.id}).then(user => res.json({status: 'Success', message: 'User last name', data: lastName}))
+        .catch((err) => res.status(400).json({status: 'Failed', message: err, data: null}));
     },
     setUserEmail: async (req, res) =>{
         const {email} = req.body;
         await userModel.findOne({_id: req.params.id}).then(user =>{
             user.email = email;
-            user.save().then(()=> res.status(200).json({'Status': 'Success', 'Message': 'Email updated!', 'Data': email}))
-            .catch((err) => res.status(400).json({'Status': 'Failed', 'Message': 'Error: ' + err, 'Data': ''}));
-        }).catch((err) => res.status(400).json({'Status': 'Failed', 'Message': 'Error: ' + err, 'Data': ''}));
+            user.save().then(()=> res.status(200).json({status: 'Success', 'Message': 'Email updated!', 'Data': user}))
+            .catch((err) => res.status(400).json({status: 'Failed', message: err, data: null}));
+        }).catch((err) => res.status(400).json({status: 'Failed', message: err, data: null}));
     },
     setUserPhone: (req, res) =>{
         const {phone} = req.body;
         userModel.findOne({_id: req.params.id}).then(user =>{
             user.phone = phone;
-            user.save().then(()=> res.status(200).json({'Status': 'Success', 'Message': 'Phone number updated!', 'Data': phone}))
-            .catch((err) => res.status(400).json({'Status': 'Failed', 'Message': 'Error: ' + err, 'Data': ''}));
-        }).catch((err) => res.status(400).json({'Status': 'Failed', 'Message': 'Error: ' + err, 'Data': ''}));
-    },
-    getUserFirstName: (req, res) =>{
-        userModel.findOne({_id: req.params.id}).then(user => res.json(user.firstName))
-        .catch((err) => res.status(400).json('err:' + err));
-    },
-    getUserLastName: (req, res) =>{
-        userModel.findOne({_id: req.params.id}).then(user => res.json(user.lastName))
-        .catch((err) => res.status(400).json('err:' + err));
+            user.save().then(()=> res.status(200).json({status: 'Success', 'Message': 'Phone number updated!', 'Data': user}))
+            .catch((err) => res.status(400).json({status: 'Failed', message: err, data: null}));
+        }).catch((err) => res.status(400).json({status: 'Failed', message: err, data: null}));
     },
     getUserEmail: (req, res) =>{
-        userModel.findOne({_id: req.params.id}).then(user => res.json(user.email))
-        .catch((err) => res.status(400).json('err:' + err));
+        userModel.findOne({_id: req.params.id}).then(user => res.json({status: 'Success', message: 'User email', data: email}))
+        .catch((err) => res.status(400).json({status: 'Failed', message: err, data: null}));
     },
     getUserPhone: (req, res) =>{
-        userModel.findOne({_id: req.params.id}).then(user => res.json(user.phone))
-        .catch((err) => res.status(400).json('err:' + err));
-    },
-    getUserAge: (req, res) =>{
-        userModel.findOne({_id: req.params.id}).then(user => res.json(user.age))
-        .catch((err) => res.status(400).json('err:' + err));
-
-    },
+        userModel.findOne({_id: req.params.id}).then(user => res.json({status: 'Success', message: 'User phone number', data: phone}))
+        .catch((err) => res.status(400).json({status: 'Failed', message: err, data: null}));
+    }, 
     setUserAge: (req, res) =>{
         const {age} = req.body;
         userModel.findOne({_id: req.params.id}).then(user =>{
             user.age = age;
             user.save().then(() =>
-            res.status(200).json(user))
-        }).catch((err) => res.status(400).json('err:' + err));
+            res.status(200).json({status: 'Success', message: 'Age updated!', data: user}))
+        }).catch((err) => res.status(400).json({status: 'Failed', message: err, data: null}));
     },
-    removeUser: (req, res) =>{
-        userModel.findByIdAndDelete({_id: req.params.id}).then(()=> res.status(200).json('User deleted!'))
-        .catch((err) => res.status(400).json('err:' + err));
+    getUserAge: (req, res) =>{
+        userModel.findOne({_id: req.params.id}).then(user => res.json({status: 'Success', message: 'User age', data: age}))
+        .catch((err) => res.status(400).json({status: 'Failed', message: err, data: null}));
     },
-
     setUserStatus: async (req, res) =>{
         const {status} = req.body;
         await userModel.findOne({_id: req.params.id}).then(users =>{
             users.status = status;
-            users.save().then(()=> res.status(200).json({status, msg:'User status updated!'}))
-            .catch((err) => res.status(400).json('err:' + err));
-        }).catch((err) => res.status(400).json('err:' + err));
+            users.save().then(()=> res.status(200).json({status: 'Success', message:'User status updated!', data: user}))
+            .catch((err) => res.status(400).json({status: 'Failed', message: err, data: null}));
+        }).catch((err) => res.status(400).json({status: 'Failed', message: err, data: null}));
     },
     getUserStatus: (req, res) =>{
-        userModel.findOne({_id: req.params.id}).then(user => res.json(user.status))
-        .catch((err) => res.status(400).json('err:' + err));
+        userModel.findOne({_id: req.params.id}).then(user => res.json({status: 'Success', message: 'User status', data: status}))
+        .catch((err) => res.status(400).json({status: 'Failed', message: err, data: null}));
     },
     setUserGender: (req, res) =>{
         const {gender} = req.body;
         userModel.findOne({_id: req.params.id}).then(user =>{
             user.gender = gender;
             user.save().then(() =>
-            res.status(200).json(user))
-        }).catch((err) => res.status(400).json('err:' + err));
+            res.status(200).json({status: 'Success', message: 'User gender updated!', data: user}))
+        }).catch((err) => res.status(400).json({status: 'Failed', message: err, data: null}));
     },
     getUserGender: (req, res) =>{
-        userModel.findOne({_id: req.params.id}).then(user => res.json(user.gender))
-        .catch((err) => res.status(400).json('err:' + err));
+        userModel.findOne({_id: req.params.id}).then(user => res.json({status: 'Success', message: 'User gender', data: gender}))
+        .catch((err) => res.status(400).json({status: 'Failed', message: err, data: null}));
 
     },
     setUserAddress: (req, res) =>{
@@ -113,33 +111,33 @@ const user = {
         userModel.findOne({_id: req.params.id}).then(user =>{
             user.address = address;
             user.save().then(() =>
-            res.status(200).json(user))
-        }).catch((err) => res.status(400).json('err:' + err));
+            res.status(200).json({status: 'Success', message: 'User address updated!', data: user}))
+        }).catch((err) => res.status(400).json({status: 'Failed', message: err, data: null}));
     },
     getUserAddress: (req, res) =>{
-        userModel.findOne({_id: req.params.id}).then(user => res.json(user.address))
-        .catch((err) => res.status(400).json('err:' + err));
+        userModel.findOne({_id: req.params.id}).then(user => res.json({status: 'Success', message: 'User address', data: address}))
+        .catch((err) => res.status(400).json({status: 'Failed', message: err, data: null}));
 
     },
     getActiveUsers: (req, res) => {
         userModel.find({ status: 'ACTIVE'})
-        .then(users => res.json(users))
-        .catch(err => res.status(400).json(`Error: ${err}`))
+        .then(users => res.json({status: 'Success', message: 'List of active users', data: users}))
+        .catch(err => res.status(400).json({status: 'Failed', message: err, data: null}))
     },
     getInActiveUsers: (req, res) => {
         userModel.find({ status: 'INACTIVE'})
-        .then(users => res.json(users))
-        .catch(err => res.status(400).json(`Error: ${err}`))
+        .then(users => res.json({status: 'Success', message: 'List of inactive users', data: users}))
+        .catch(err => res.status(400).json({status: 'Failed', message: err, data: null}))
     },
     getInternUsers: (req, res) => {
         userModel.find({ level: "intern"})
-        .then(users => res.json(users))
-        .catch(err => res.status(400).json(`Error: ${err}`))
+        .then(users => res.json({status: 'Success', message: 'List of interns', data: users}))
+        .catch(err => res.status(400).json({status: 'Failed', message: err, data: null}))
     },
     getMentorUsers: (req, res) => {
         userModel.find({ level: "mentor"})
-        .then(users => res.json(users))
-        .catch(err => res.status(400).json(`Error: ${err}`))
+        .then(users => res.json({status: 'Success', message: 'List of mentors', data: users}))
+        .catch(err => res.status(400).json({status: 'Failed', message: err, data: null}))
     }
 };
 
