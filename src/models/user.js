c884c9496f50d03d089f9e5afdd9d854cc5bf068
Yusuf Abdulrahman <48928718+devYusuf@users.noreply.github.com>
2020-06-10 15:01:28 +0000

    Update user.js

--- src/models/user.js
+++ src/models/user.js
@@ -1,67 +1,66 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
+const mongoose = require("mongoose");
+const validator = require("validator");
 
-
-const User = mongoose.model('User', {
-    firstName: {
-        type: String,
-        required: true,
-        trim: true
+const User = mongoose.model("User", {
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        //subject to change to a custom response later
+        throw new Error("Invalid Email");
+      }
     },
-    lastName: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        lowercase: true,
-        validate(value){
-            if (!validator.isEmail(value)){
-                //subject to change to a custom response later
-                throw new Error('Invalid Email');
-            }
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+    validate(value) {
+      if (!/^\d{11,13}$/.test(value)) {
+        if (/[^\d]/.test(value)) {
+          throw new Error("One or more invalid characters. Numbers only");
         }
+        //subject to change to a custom response later
+        throw new Error("Phone number should consist of 11 to 13 numbers");
+      }
     },
-    phoneNumber: {
-        type: String,
-        required: true,
-        validate(value){
-                if(!/^\d{11,13}$/.test(value)){
-                    if(/[^\d]/.test(value)){
-                        throw new Error('One or more invalid characters. Numbers only');
-                    }
-                //subject to change to a custom response later
-                throw new Error('Phone number should consist of 11 to 13 numbers');
-            }
-        }
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive number");
+      }
     },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value){
-			if(value < 0) {
-                throw new Error('Age must be a positive number')
-            }
-		}
-    },
-    address: {
-        type: String,
-        trim: true,
-    },
-    gender: { 
-        type: String,
-        enum: ['male', 'female'],
-        required: true
-    },
-    status: { 
-        type: String,
-        enum: ['ACTIVE', 'INACTIVE'],
-        default: 'ACTIVE'
-    }
+  },
+  address: {
+    type: String,
+    trim: true,
+  },
+  gender: {
+    type: String,
+    enum: ["male", "female"],
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["active", "inactive"],
+    default: "active",
+  },
 });
 
 module.exports = User;
