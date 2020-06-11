065b962f1044a7a9eb206df1a83458672a3f9067
Amarachi9 <59516730+Amarachi9@users.noreply.github.com>
2020-06-11 21:54:34 +0100

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -77,7 +77,7 @@ const user = {
         userModel.findOne({_id: req.params.id}).then(user => res.json({status: 'Success', message: 'User phone number', data: user.phone}))
         .catch((err) => res.status(400).json({status: 'Failed', message: 'An Error Occurred', data: null}));
     }, 
-    changetUserEmail: (req, res) =>{
+    changeUserEmail: (req, res) =>{
         userModel.findOne({_id: req.params.id}).then(user => res.json({status: 'Success', message: 'User email', data: user.email}))
         .catch((err) => res.status(400).json({status: 'Failed', message: 'An Error Occurred', data: null}));
     },
