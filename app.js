895458e2a4659b93c03704a86475a0d1bd08be83
Yusuf Abdulrahman <48928718+devYusuf@users.noreply.github.com>
2020-06-11 15:46:12 +0000

    Update the api base url to /v1/
    Update the api base url to /v1/ and the postman tutorial page to /postman

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const path = require('path');
 const dotenv = require('dotenv');
 const helmet = require('helmet');
 const router =  require('./src/routes/routes');
-const openApiDocumentation = require('./src/swagger/openApiDocumentation')
-const swaggerUi = require('swagger-ui-express')
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -14,15 +12,13 @@ import connectToDatabase  from'./src/db/mongoose';
 dotenv.config();
 connectToDatabase();
 
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiDocumentation));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use('/src', express.static('img'))
-app.get('/', (req, res) =>{
+app.get('/postman', (req, res) =>{
     res.sendFile(path.join(__dirname, '/src/public', 'index.html'));
 });
-app.use('/', router);
+app.use('/v1', router);
 
 app.listen(port, () => console.log(`Team Granite App is running on port: ${port}`));
