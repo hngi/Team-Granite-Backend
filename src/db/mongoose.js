491462723557481efccfc11e667184ec3dd5feeb
Ruberandinda Patience <ruberandindap@gmail.com>
2020-06-12 08:54:53 +0200

    remove conflict not fromt mongoose.js file

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
-// import mongoose from 'mongoose';
 const mongoose = require('mongoose');
-=======
-import mongoose from 'mongoose';
-import env from 'dotenv';
+
+const dotenv = require('dotenv');
 
 env.config();
-
->>>>>>> dedd6338abfa4e123ef1fa2ca6203c1b4b9fbe3a
 const connectToDatabase = () => {
     mongoose.connect(process.env.ATLAS_URI, {
         useUnifiedTopology: true,
@@ -21,10 +16,4 @@ const connectToDatabase = () => {
     mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
 };
 
-<<<<<<< HEAD
-// mongodb://127.0.0.1:27017/Team-Granite-Dockerized-Users-Management-App-db',
-=======
-// mongodb://127.0.0.1:27017/Team-Granite-Dockerized-Users-Management-App-db,
->>>>>>> dedd6338abfa4e123ef1fa2ca6203c1b4b9fbe3a
-
-export default connectToDatabase;
+module.exports= connectToDatabase;
